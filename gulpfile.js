4c313e367f32cd0b9d73cdf03e27d3bd34ec2314
mplocki <plockione@gmail.com>
2016-04-03 17:41:00 +0200

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,36 @@
+'use strict';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+var jade = require('gulp-jade');
+
+
+gulp.task('templates', function() {
+  gulp.src('./src/jade/*.jade')
+    .pipe(jade({
+      pretty: true,
+      locals: {
+        title: 'My website'
+      }
+    }))
+    .pipe(gulp.dest('./dist/'));
+});
+
+gulp.task('templates:watch', ['templates'], function () {
+  gulp.watch('./src/jade/**/*.jade', ['templates']);
+});
+
+
+gulp.task('sass', function () {
+  return gulp.src('./src/sass/**/*.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('./dist/css'));
+});
+
+gulp.task('sass:watch', ['sass'], function () {
+  gulp.watch('./src/sass/**/*.scss', ['sass']);
+});
+
+gulp.task('default', ['sass:watch', 'templates:watch']);
